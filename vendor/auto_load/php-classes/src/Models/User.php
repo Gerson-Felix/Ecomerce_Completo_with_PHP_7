7cafe99bb5411dc00741a6be61342dcf2de66c94
Gerson-Felix <gerson.01@outlook.pt>
2020-06-08 03:06:48 +0100

    Criação do Registrar Cliente

--- vendor/auto_load/php-classes/src/Models/User.php
+++ vendor/auto_load/php-classes/src/Models/User.php
@@ -275,9 +275,29 @@
 			$_SESSION[User::ERROR_REGISTER] = $msg;
 		}
 
+		public static function getErrorRegister()
+		{
+			$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+			User::clearError();
+
+			return $msg;
+		}
+
 		public static function getPasswordHash ($password)
 		{
 			return password_hash($password, PASSWORD_DEFAULT, ['cost'=>12]);
+		}
+
+		public static function checkLoginExist ($login)
+		{
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+				':deslogin'=>$login
+			]);
+
+			return (count($results) > 0);
 		}
 	}
 ?>
