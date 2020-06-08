c665494021be2a5ce2fff6544c6e5ae2061a6e6e
Gerson-Felix <gerson.01@outlook.pt>
2020-06-09 00:10:46 +0100

    Finalização do Pedido com a geração da Factura

--- site.php
+++ site.php
@@ -6,6 +6,8 @@
 	use \All\Models\Cart;
 	use \All\Models\Address;
 	use \All\Models\User;
+	use \All\Models\Order;
+	use \All\Models\OrderStatus;
 
 	$app->get('/', function() {
     	
@@ -130,8 +132,6 @@
 
 		$cart = Cart::getFromSession();
 
-		$address = new Address();
-
 		$page = new Page();
 
 		$page->setTpl("checkout", [
@@ -144,7 +144,25 @@
 
 		User::verifyLogin(false);
 
-		header("Location: /order");
+		$user = User::getFromSession();
+
+		$order = new Order();
+
+		$cart = Cart::getFromSession();
+
+		$totals = $cart->getProductsTotals();
+
+		$order->setData([
+
+			'idcart'=>$cart->getidcart(),
+			'iduser'=>$user->getiduser(),
+			'idstatus'=>OrderStatus::EM_ABERTO,
+			'vltotal'=>$totals['vlprice']
+		]);
+
+		$order->save();
+
+		header("Location: /order/".$order->getidorder());
 
 		exit();
 	});
@@ -368,5 +386,88 @@
 		header('Location: /profile');
 
 		exit();
+	});
+
+	$app->get('/order/:idorder', function ($idorder) {
+
+		User::verifyLogin(false); 
+
+		$order = new Order();
+
+		$order->get((int)$idorder);
+
+		$page = new Page();
+
+		$page->setTpl("payment", [
+
+			'order'=>$order->getValues()
+		]);
+	});
+
+	$app->get('/boleto/:idorder', function($idorder) {
+
+		User::verifyLogin(false);
+
+		$order = new Order();
+
+		$order->get((int)$idorder);
+
+		// DADOS DO BOLETO PARA O SEU CLIENTE
+		$dias_de_prazo_para_pagamento = 10;
+		$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+		$valor_cobrado = $order->getvltotal(); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+		$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+		$valor_boleto=number_format($valor_cobrado, 2, ',', '');
+
+		$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
+		$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
+		$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+		$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+		$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+		$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+
+		// DADOS DO SEU CLIENTE
+		$dadosboleto["sacado"] = $order->getdesperson();
+
+		// INFORMACOES PARA O CLIENTE
+		$dadosboleto["demonstrativo1"] = "Pagamento de Compra no E-commerce";
+		$dadosboleto["demonstrativo2"] = "";
+		$dadosboleto["demonstrativo3"] = "";
+		$dadosboleto["instrucoes1"] = "";
+		$dadosboleto["instrucoes2"] = "";
+		$dadosboleto["instrucoes3"] = "";
+		$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema E-commerce";
+
+		// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+		$dadosboleto["quantidade"] = "";
+		$dadosboleto["valor_unitario"] = "";
+		$dadosboleto["aceite"] = "";		
+		$dadosboleto["especie"] = "Akz";
+		$dadosboleto["especie_doc"] = "";
+
+
+		// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+
+
+		// DADOS DA SUA CONTA - ITAÚ
+		$dadosboleto["agencia"] = ""; // Num da agencia, sem digito
+		$dadosboleto["conta"] = "";	// Num da conta, sem digito
+		$dadosboleto["conta_dv"] = ""; 	// Digito do Num da conta
+
+		// DADOS PERSONALIZADOS - ITAÚ
+		$dadosboleto["carteira"] = "";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+
+		// SEUS DADOS
+		$dadosboleto["identificacao"] = "GCTecnology";
+		$dadosboleto["cpf_cnpj"] = "";
+		$dadosboleto["endereco"] = "Rua do Shopping Kinaxixi";
+		$dadosboleto["cidade_uf"] = "Luanda";
+		$dadosboleto["cedente"] = "";
+
+		// NÃO ALTERAR!
+		$path = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."res".DIRECTORY_SEPARATOR."boletophp".DIRECTORY_SEPARATOR."include".DIRECTORY_SEPARATOR;
+
+		require_once($path. "funcoes_itau.php");
+		require_once($path. "layout_itau.php");
 	});
 ?>
---
+++ vendor/auto_load/php-classes/src/Models/Order.php
@@ -0,0 +1,41 @@
+<?php
+	namespace All\Models;
+
+	use \All\DB\Sql;
+	use \All\Model;
+
+	class Order extends Model
+	{
+		public function save()
+		{
+			$sql = new Sql();
+
+			$results = $sql->select("CALL sp_orders_save (:idorder, :idcart, :iduser, :idstatus, :vltotal)", [
+				':idorder'=>$this->getidorder(),
+				':idcart'=>$this->getidcart(),
+				':iduser'=>$this->getiduser(),
+				':idstatus'=>$this->getidstatus(),
+				':vltotal'=>$this->getvltotal()
+			]);
+
+			if (count($results[0]) > 0)
+			{
+				$this->setData($results[0]);
+			}
+		} 
+
+		public function get ($idorder)
+		{
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_orders a INNER JOIN tb_ordersstatus b USING (idstatus) INNER JOIN tb_carts c USING (idcart) INNER JOIN tb_users d ON d.iduser = a.iduser INNER JOIN tb_persons e ON e.idperson = d.idperson WHERE a.idorder = :idorder", [
+				':idorder'=>$idorder
+			]);
+
+			if (count($results[0]) > 0)
+			{
+				$this->setData($results[0]);
+			}
+		}
+	}
+?>
