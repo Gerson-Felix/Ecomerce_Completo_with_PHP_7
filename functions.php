46e499fc2a046bea7705a179ae5c427d6fa5061d
Gerson-Felix <gerson.01@outlook.pt>
2020-06-09 03:09:23 +0100

    Controle dos Pedidos

--- functions.php
+++ functions.php
@@ -7,6 +7,11 @@
 		return number_format($vlprice, 2, ",", ".");
 	}
 
+	function formatDate($date)
+	{
+		return date('d/m/Y', strtotime($date));
+	}
+
 	function checkLogin($inadmin = true)
 	{
 		return User::checkLogin($inadmin);
@@ -34,6 +39,6 @@
 
 		$totals = $cart->getProductsTotals();
 
-		return formatPrice($totals['vlprice']);
+		return ($totals['vlprice'] != '') ? formatPrice($totals['vlprice']) : 0.0;
 	}
 ?>
--- vendor/auto_load/php-classes/src/Models/Order.php
+++ vendor/auto_load/php-classes/src/Models/Order.php
@@ -6,6 +6,9 @@
 
 	class Order extends Model
 	{
+		const SUCESS = "Order-Sucess";
+		const ERROR = "Order-Error";
+
 		public function save()
 		{
 			$sql = new Sql();
@@ -36,6 +39,69 @@
 			{
 				$this->setData($results[0]);
 			}
+		}
+
+		public function listAll()
+		{
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_orders a INNER JOIN tb_ordersstatus b USING (idstatus) INNER JOIN tb_carts c USING (idcart) INNER JOIN tb_users d ON d.iduser = a.iduser INNER JOIN tb_persons e ON e.idperson = d.idperson ORDER BY a.dtregister DESC");
+		}
+
+		public function delete()
+		{
+			$sql = new Sql();
+
+			$sql->query("DELETE FROM tb_orders WHERE idorder = :idorder", [
+				':idorder'=>$this->getidorder()
+			]);
+		}
+
+		public function getCart():Cart
+		{
+			$cart = new Cart();
+
+			$cart->get((int)$this->getidcart());
+
+			return $cart;
+		}
+
+		public static function setError($msg)
+		{
+			$_SESSION[Order::ERROR] = $msg;
+		}
+
+		public static function getError()
+		{
+			$msg = (isset($_SESSION[Order::ERROR]) && $_SESSION[Order::ERROR]) ? $_SESSION[Order::ERROR] : '';
+
+			Order::clearError();
+
+			return $msg;
+		}
+
+		public static function clearError()
+		{
+			$_SESSION[Order::ERROR] = NULL;
+		}
+
+		public static function setSuccess($msg)
+		{
+			$_SESSION[Order::SUCESS] = $msg;
+		}
+
+		public static function getSuccess()
+		{
+			$msg = (isset($_SESSION[Order::SUCESS]) && $_SESSION[Order::SUCESS]) ? $_SESSION[Order::SUCESS] : '';
+
+			Order::clearError();
+
+			return $msg;
+		}
+
+		public static function clearSucess()
+		{
+			$_SESSION[Order::SUCESS] = NULL;
 		}
 	}
 ?>
